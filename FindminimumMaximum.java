e52df3fa29f4f8c099e9347de634230ebcd4bb4f
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-09-26 23:02:31 +0530

    Add files via upload

---
+++ FindminimumMaximum.java
@@ -0,0 +1,29 @@
+package Array_topic;
+
+public class FindminimumMaximum {
+    public static int minElement(int arr[]) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+        return min;
+    }
+
+    public static int maxElement(int arr[]) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 44, 5, 6, 7, 5, 44, 5, 6, 77, 88, 34, 56, 78, 90, 98, 767, 54, 32, 12, 34, 56, 566, 6675, 77,
+                45, 456, 35, 345, 2 };
+        // System.out.println(minElement(arr));
+        System.out.println(maxElement(arr));
+    }
+}
