f27ca0cdf73e03d39e8610e539bdc7e08c9383cf
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-09-27 16:37:01 +0530

    Add files via upload

--- Rotatearray_by_k.java
+++ Rotatearray_by_k.java
@@ -3,10 +3,10 @@ package Array_topic;
 public class Rotatearray_by_k {
 
     public static int[] rotatebyK(int arr[], int k) {
-        if (k > arr.length) {
+        if (k > arr.length - 1 / 2) {
             return arr;
         } else {
-            for (int i = 0, j = k - 1; i < (k / 2); i++, j--) {
+            for (int i = 0, j = arr.length - 1; i <= k; i++, j--) {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
@@ -17,7 +17,7 @@ public class Rotatearray_by_k {
 
     public static void main(String[] args) {
         int arr[] = { 2, 5, 1, 3, 7, 9, 8, 4 };
-        int arr2[] = rotatebyK(arr, 4);
+        int arr2[] = rotatebyK(arr, 2);
         for (int i : arr2) {
             System.out.print(i + " ");
         }
