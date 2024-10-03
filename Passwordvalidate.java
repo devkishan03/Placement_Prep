45dc5c11daf91f81fbda934513f6722f57e5fd70
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-10-03 15:07:35 +0530

    Add files via upload

--- Passwordvalidate.java
+++ Passwordvalidate.java
@@ -1,12 +1,12 @@
 
 public class Passwordvalidate {
     public static boolean isValid(String str) {
-        String rgx = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&^*~]).{8,20}";
+        String rgx = "^(?=\\S+$)(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&^*~]).{8,20}$";
         return str.matches(rgx);
     }
 
     public static void main(String[] args) {
-        String pass = "K1ishan!jhfg)";
+        String pass = "K1ishan!jhfg";
         System.out.println(isValid(pass));
     }
 }
