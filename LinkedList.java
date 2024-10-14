3826cdf8c3e1c43b16331ff524634b0abd76145b
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-10-14 11:17:32 +0530

    Add files via upload

--- LinkedList.java
+++ LinkedList.java
@@ -13,6 +13,7 @@ public class LinkedList {
 
     Node head = null;
     Node tail = null;
+    int size = 0;
 
     public LinkedList() {
 
@@ -26,11 +27,12 @@ public class LinkedList {
             Node temp = new Node(arr[0]);
             head = temp;
             tail = temp;
+            size++;
             for (int i = 1; i < length; i++) {
                 temp = new Node(arr[i]);
                 tail.next = temp;
                 tail = temp;
-
+                size++;
             }
         }
     }
@@ -43,29 +45,76 @@ public class LinkedList {
             tail.next = temp;
             tail = temp;
         }
+        size++;
     }
 
     public void insertAtHead(int val) {
         Node temp = new Node(val);
         if (head == null) {
             // insertAtEnd(n);
-            head = temp;
-            tail = head;
+            head = tail = temp;
         } else {
             temp.next = head;
             head = temp;
         }
+        size++;
+    }
 
+    public void insertAt(int index, int val) {
+        Node t = new Node(val);
+        if (head == null && index > 0) {
+            System.out.println("List is empty");
+            return;
+        } else if (index < 0) {
+            System.out.println("Index will be positive");
+            return;
+        } else if (index > size()) {
+            System.out.println("Index out of bound");
+            return;
+        } else if (index == 0) {
+            insertAtHead(val);
+            return;
+        } else if (size() == index) {
+            insertAtEnd(val);
+            return;
+        } else {
+            Node temp = head;
+            for (int i = 1; i < index; i++) {
+                temp = temp.next;
+            }
+            t.next = temp.next;
+            temp.next = t;
+            size++;
+        }
+    }
+
+    public int getAt(int index) {
+        if (head == null) {
+            System.out.println("List is empty");
+            return -1;
+        } else if (index == 0) {
+            return head.data;
+        } else if (index >= size()) {
+            System.out.println("Index out of bound");
+            return -1;
+        } else {
+            Node temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            return temp.data;
+        }
     }
 
     public int size() {
-        int i = 0;
-        Node temp = head;
-        while (temp != null) {
-            i++;
-            temp = temp.next;
-        }
-        return i;
+        // int i = 0;
+        // Node temp = head;
+        // while (temp != null) {
+        // i++;
+        // temp = temp.next;
+        // }
+        // return i;
+        return size;
     }
 
     public void display() {
@@ -90,18 +139,27 @@ public class LinkedList {
 
         // ll.display();
         ll.insertAtHead(1);
-        ll.display();
+        // ll.display();
 
         ll.insertAtEnd(11);
         ll.insertAtHead(111);
+        // ll.display();
+
+        // LinkedList ll2 = new LinkedList();
+        // ll2.insertAtHead(1);
+        // ll2.insertAtEnd(2);
+        // // ll2.createList(arr, n);
+        // ll2.display();
+        ll.insertAt(0, 21);
+        // ll2.insertAtHead(2);
+        // ll2.insertAt(1, 1);
+        // ll.display();
+        // ll2.display();
+        // ll2.insertAt(3, 3);
+        // System.out.println(ll2.tail.data);
         ll.display();
-
-        LinkedList ll2 = new LinkedList();
-        ll2.insertAtHead(1);
-        ll2.insertAtEnd(2);
-        // ll2.createList(arr, n);
-        ll2.display();
-
+        System.out.println(ll.size());
+        System.out.println(ll.getAt(8));
     }
 
 }
