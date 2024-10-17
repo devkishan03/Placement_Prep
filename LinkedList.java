eab12f579078ba2dd0148050ab1cb6af96f1d20e
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-10-17 20:03:43 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import java.io.FileReader;
+
 class Node {
     int data;
     Node next = null;
@@ -11,9 +13,9 @@ class Node {
 
 public class LinkedList {
 
-    Node head = null;
-    Node tail = null;
-    int size = 0;
+    private Node head = null;
+    private Node tail = null;
+    private int size = 0;
 
     public LinkedList() {
 
@@ -226,6 +228,126 @@ public class LinkedList {
         size--;
     }
 
+    public boolean isSorted() {
+        Node temp = head;
+        int tempval = Integer.MIN_VALUE;
+        while (temp != null) {
+            if (temp.data < tempval) {
+                return false;
+            }
+            tempval = temp.data;
+            temp = temp.next;
+        }
+        return true;
+    }
+
+    public void removeDuplicates_Sorted() {
+        Node current = head;
+        Node next = head.next;
+        while (next != null) {
+            if (current.data != next.data) {
+                current = next;
+                next = next.next;
+            } else {
+                current.next = next.next;
+                next = current.next;
+            }
+
+        }
+    }
+
+    public void reverseByElement() {
+        int arr[] = new int[size];
+        Node temp = head;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = temp.data;
+            temp = temp.next;
+        }
+        temp = head;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            temp.data = arr[i];
+            temp = temp.next;
+        }
+    }
+
+    public void reverseByNode() {
+        Node current = head;
+        Node prev = null;
+        Node prev2 = null;
+        while (current != null) {
+            prev2 = prev;
+            prev = current;
+            current = current.next;
+            prev.next = prev2;
+        }
+        head = prev;
+    }
+
+    private void reverse(Node p, Node q) {
+        if (p != null) {
+            reverse(p.next, p);
+            p.next = q;
+        } else {
+            head = q;
+        }
+    }
+
+    public void recursiveReverse() {
+        Node p = head;
+        Node q = null;
+        reverse(p, q);
+
+    }
+
+    public void concat(Node second) {
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = second;
+    }
+
+    public void margeWith(Node other) {
+        Node first = head;
+        Node second = other;
+        Node newlist = null;
+        Node last = null;
+        if (first.data < second.data) {
+            newlist = first;
+            last = first;
+            first = first.next;
+        } else {
+            newlist = second;
+            last = second;
+            second = second.next;
+        }
+        while (first != null && second != null) {
+            if (first.data < second.data) {
+                last.next = first;
+                last = first;
+                first = first.next;
+            } else if (first.data >= second.data) {
+                last.next = second;
+                last = second;
+                second = second.next;
+            }
+        }
+        while (first != null) {
+            last.next = first;
+            last = first;
+            first = first.next;
+
+        }
+        while (second != null) {
+            last.next = second;
+            last = second;
+            second = second.next;
+
+        }
+        head = newlist;
+
+    }
+
     public void display() {
         Node temp;
         temp = head;
@@ -241,41 +363,57 @@ public class LinkedList {
         LinkedList ll = new LinkedList();
         // ll.insertAtEnd(2);
         // ll.insertAtEnd(5);
-        int arr[] = { 2, 4, 6, 8 };
-        int length = 4;
-        ll.createList(arr, length);
-        ll.insertAtEnd(10);
+        // int arr[] = { 2, 4, 6, 8 };
+        // int length = 4;
+        // ll.createList(arr, length);
+        // ll.insertAtEnd(10);
 
         // ll.display();
-        ll.insertAtHead(1);
+        // ll.insertAtHead(1);
         // ll.display();
 
-        ll.insertAtEnd(11);
-        ll.insertAtHead(111);
+        // ll.insertAtEnd(11);
+        // ll.insertAtHead(111);
         // ll.display();
 
-        // LinkedList ll2 = new LinkedList();
-        // ll2.insertAtHead(1);
-        // ll2.insertAtEnd(2);
+        LinkedList ll2 = new LinkedList();
+        ll2.insertAtHead(1);
+        ll2.insertAtEnd(3);
+        ll2.insertAtEnd(5);
+        ll2.insertAtEnd(6);
         // // ll2.createList(arr, n);
         // ll2.display();
-        ll.insertAt(0, 21);
+        // ll.insertAt(0, 21);
         // ll2.insertAtHead(2);
         // ll2.insertAt(1, 1);
         // ll.display();
         // ll2.display();
         // ll2.insertAt(3, 3);
         // System.out.println(ll2.tail.data);
-        ll.display();
+        // ll.display();
         // System.out.println(ll.size());
         // System.out.println(ll.getAt(8));
         // System.out.println(ll.maxElement());
         // System.out.println(ll.minElement());
         // System.out.println(ll.searchElement2(1).data);
         // ll.deleteByElement(21);
-        ll.deleteByIndex(8);
+        // ll.deleteByIndex(8);
+        // ll.display();
+        // System.out.println(ll.tail.data);
+        int arr[] = { 0, 2, 4, 7, 8, 9 };
+        int n = 6;
+        ll.createList(arr, n);
         ll.display();
-        System.out.println(ll.tail.data);
+        // System.out.println(ll.size);
+        // ll.removeDuplicates_Sorted();
+        // ll.reverseByElement();
+        // ll.display();
+        // ll.reverseByNode();
+        // ll.recursiveReverse();
+        ll2.display();
+        ll.margeWith(ll2.head);
+        ll.display();
+        // System.out.println(ll.isSorted());
     }
 
 }
