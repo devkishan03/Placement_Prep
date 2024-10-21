6c836ae63c2c3b4398a854ad562f142019fb5faa
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-10-21 11:22:07 +0530

    Add files via upload

--- DubleeLinkedList.java
+++ DubleeLinkedList.java
@@ -1,34 +1,31 @@
-package LinkedList;
+package LinkList2;
 
-class DNode {
-    int data;
-    DNode next;
-    DNode previous;
+class Node {
+    public int data;
+    public Node next;
+    public Node previous;
 
-    public DNode(int val) {
+    public Node(int val) {
         data = val;
     }
-
 }
 
 public class DubleeLinkedList {
-    DNode head = null;
-    DNode tail = null;
-    int size = 0;
+    public Node head;
+    public Node tail;
+    public int size;
 
     public DubleeLinkedList() {
 
     }
 
     public DubleeLinkedList(int arr[], int n) {
-
-        DNode temp = new DNode(arr[0]);
+        Node temp = new Node(arr[0]);
         head = temp;
         tail = head;
         size++;
         for (int i = 1; i < n; i++) {
-            temp = new DNode(arr[i]);
-            temp.next = tail.next;
+            temp = new Node(arr[i]);
             temp.previous = tail;
             tail.next = temp;
             tail = temp;
@@ -37,8 +34,51 @@ public class DubleeLinkedList {
 
     }
 
+    public void insertAtEnd(int val) {
+        Node temp = new Node(val);
+        if (head != null) {
+            tail.next = temp;
+            temp.previous = tail;
+            tail = temp;
+        } else {
+            head = tail = temp;
+        }
+        size++;
+    }
+
+    public void insertAtHead(int val) {
+        Node temp = new Node(val);
+        if (head != null) {
+            head.previous = temp;
+            temp.next = head;
+            head = temp;
+        } else {
+            head = tail = temp;
+        }
+        size++;
+    }
+
+    public void deleteHead() {
+        if (head == null) {
+            return;
+        }
+        if (head.next != null) {
+            Node temp = head;
+            temp.next.previous = null;
+            head = temp.next;
+            temp = null;
+        } else {
+            head = tail = null;
+        }
+        size--;
+    }
+
     public void display() {
-        DNode temp = head;
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = head;
         while (temp != null) {
             System.out.print(temp.data + " ");
             temp = temp.next;
@@ -47,7 +87,11 @@ public class DubleeLinkedList {
     }
 
     public void reverseDisplay() {
-        DNode temp = tail;
+        if (tail == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = tail;
         while (temp != null) {
             System.out.print(temp.data + " ");
             temp = temp.previous;
@@ -55,12 +99,42 @@ public class DubleeLinkedList {
         System.out.println();
     }
 
+    public void deleteEnd() {
+        if (tail == null) {
+            return;
+        }
+        if (tail.previous != null) {
+            tail = tail.previous;
+            tail.next = null;
+        } else {
+            head = tail = null;
+        }
+        size--;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5 };
         int n = 5;
-        DubleeLinkedList d = new DubleeLinkedList(arr, n);
-        d.display();
-        d.reverseDisplay();
+        DubleeLinkedList dl = new DubleeLinkedList(arr, n);
+        // dl.display();
+        // dl.insertAtEnd(10);
+        // dl.insertAtHead(11);
+        // dl.reverseDisplay();
+        // System.out.println(dl.size);
+        DubleeLinkedList dl2 = new DubleeLinkedList();
+        dl2.insertAtEnd(10);
 
+        dl2.insertAtHead(11);
+        dl2.display();
+        System.out.println(dl2.size);
+        dl2.deleteHead();
+        dl2.display();
+        // System.out.println(dl2.head.previous.data);
+        System.out.println(dl2.head.previous);
+        System.out.println(dl2.size);
+        dl2.display();
+        // dl2.deleteHead();
+        dl2.deleteEnd();
+        dl2.display();
     }
 }
